45a0e52b079a263f3fafc36abcebe874bb775beb
Jin Choo <jinchoo315@gmail.com>
2020-11-24 23:49:38 -0600

    This is save point and application works, still need to add ReadMe for this homeowork

--- index.js
+++ index.js
@@ -1,69 +1,75 @@
 // array of questions for user
-const inquirer = require('inquirer');
-const fs = require('fs')
-
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name:  'username',
-            message:  'What is your GitHub username?'
-        },
-        {
-            type:  'input',
-            name:  'email',
-            message:  'What is your email address?',
-        },
-        {
-            type: 'input',
-            name:  'project',
-            message:  "What is your project's name?"
-        },
-        {
-            type:  'input',
-            name:  'description',
-            message:  'Write a short description of your project?'
-        },
-        {
-            type:  'list',
-            message:  'What kind of license should your project have?',
-            name:  'license',
-            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-        },
-        {
-            type:  'input',
-            name:  'dependencies',
-            message:  'What command should be run to install dependencies? npm i',
-        },
-        {
-            type:  'input',
-            name:  'tests',
-            message:  'What command should be run to run tests? npm tests',
-        },
-        {
-            type:  'input',
-            name:  'repo',
-            message:  'What does the user need to know about using the repo?'
-        },
-        {
-            type:  'input',
-            name:  'contributing',
-            message:  'What does the user need to know about contributing to the repo?'
-        }
-    ])
-    .then(function(answers) {
-        console.log(answers);
-    })
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // // function to write README file
-// function writeToFile(fileName, data) {
-// }
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Success!  Your README.md file has been generated");
+  });
+}
 
 // // function to initialize program
-// function init() {
-
-// }
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?",
+      },
+      {
+        type: "input",
+        name: "repo",
+        message: "What does the user need to know about using the repo?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "project",
+        message: "What is your project's name?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Write a short description of your project?",
+      },
+      {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+      },
+      {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to install dependencies?",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to run tests?",
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message:
+          "What does the user need to know about contributing to the repo?",
+      },
+    ])
+    .then(function (answers) {
+      writeToFile("README2.md", generateMarkdown(answers));
+      console.log(answers);
+    });
+}
 
 // // function call to initialize program
-// init();
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,45 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `
+# ${data.project}
+  
+${data.description};
+  
+## Installation
+  
 
+\`\`\`bash
+${data.dependencies};
+\`\`\`
+  
+## How to run the test
+  
+\`\`\`bash
+${data.tests};
+\`\`\`
+
+  
+## Contributing
+
+${data.contributing};
+
+
+## License
+
+${data.license};
+
+
+## Repository
+
+Username: ${data.username};
+${data.repo};
+
+
+## Contact
+
+Contact me at ${data.email};
 `;
 }
+
 
 module.exports = generateMarkdown;
